ec3ed1a4c67aaaea0dea44d2e0a50884144b3b2a
Mehdi Hasan <mhasan@omicronlab.com>
2021-04-25 11:31:08 +0200

    Add demo cli

---
+++ cmd/avrophoneticdemo/avrophoneticdemo.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/mugli/libavrophonetic/pkg/databasedconv"
+	"github.com/mugli/libavrophonetic/pkg/rulebasedconv"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		printUsage()
+		os.Exit(0)
+	}
+
+	arg := os.Args[1]
+
+	rulebasedConverter := rulebasedconv.NewConverter()
+
+	databasedConverter, err := databasedconv.NewConverter()
+	if err != nil {
+		fmt.Printf("Error reading datafiles %v\n", err)
+		os.Exit(1)
+	}
+
+	rulebasedConversion := rulebasedConverter.ConvertWord(arg)
+	databasedConversion := databasedConverter.ConvertWord(arg)
+
+	fmt.Printf("(Rulebased conversion) %s = %s \n", arg, rulebasedConversion)
+	fmt.Printf("(Databased conversion) %s = %+v \n", arg, databasedConversion)
+}
+
+func printUsage() {
+	help := `
+Pass a word as argument:
+
+avrophoneticdemo shadhinota`
+
+	fmt.Println(help)
+}
